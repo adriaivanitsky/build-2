39b6de2641ae82ff5b6afd808a808e1127579099
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-15 13:46:06 -0700

    trying to add image to list

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import { food } from '/food.js';
 for (let foodItem of food) {
     const foodItem = document.createElement('li');
     const foodList = document.createElement('ul');
+    const img = document.createElement('img');
+    img src = 
     
     foodItem.textContent = food.name;
     foodList.textContent = food;
