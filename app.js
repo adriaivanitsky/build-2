1ba3151580f5609953eb014178a353b3dbcf3de4
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-15 13:53:30 -0700

    yeah im stuck

--- app.js
+++ app.js
@@ -4,12 +4,8 @@ for (let foodItem of food) {
     const foodItem = document.createElement('li');
     const foodList = document.createElement('ul');
     const img = document.createElement('img');
-    img src = 
+    img src = "./avocado.png"
     
     foodItem.textContent = food.name;
     foodList.textContent = food;
-
-
-    
-
 }
