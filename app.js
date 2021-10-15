0bb081e2051766e1766d513fa25241513bcef9f6
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-15 13:44:21 -0700

    feel confused about how to write what i want to happen

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import { food } from '/food.js';
 for (let foodItem of food) {
     const foodItem = document.createElement('li');
     const foodList = document.createElement('ul');
-    console.log(foodItem)
+    
+    foodItem.textContent = food.name;
+    foodList.textContent = food;
 
 
     
